1b3077aaa92001bcb9ed177297235f42d8e8d220
Turánszki János <turanszkij@gmail.com>
2026-07-19 15:40:44 +0200

    ctrl delete

--- mini-ai.cpp
+++ mini-ai.cpp
@@ -13,6 +13,9 @@
 #include <wininet.h>
 #pragma comment(lib, "wininet.lib")
 
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+
 #include <thread>
 #include <string>
 #include <fstream>
@@ -817,6 +820,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
 	_wgetcwd(originalWorkingDir, MAX_PATH); // save original working dir at startup
 
+	CoInitialize(NULL);
+
 	static bool exiting = false;
 	static auto WndProc = [](HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) -> LRESULT
 		{
@@ -1322,7 +1327,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
 	window = CreateWindowW(L"mini-ai", L"mini-ai", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, window_width, window_height, nullptr, nullptr, NULL, nullptr);
 	hEdit = CreateWindowW(L"EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL | ES_WANTRETURN | WS_VSCROLL, 0, 0, 0, 0, window, NULL, hInstance, NULL);
-	//Edit_SetCueBannerTextFocused(hEdit, L"Type image description...", TRUE);
+	SHAutoComplete(hEdit, SHACF_AUTOAPPEND_FORCE_OFF | SHACF_AUTOSUGGEST_FORCE_OFF);
 
 	// Create UI Button Controls matching native modern font layouts
 	hBtnLoad = CreateWindowW(L"BUTTON", L"\xE8B7", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 0, 0, 0, 0, window, (HMENU)IDC_LOAD_BUTTON, hInstance, NULL);
